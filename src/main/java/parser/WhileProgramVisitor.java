9561d0c19e2e4d84dc1ecabbfe096798f2cc92bc
Zsolt Tabi <zsolt.tabi@ericsson.com>
2017-06-09 09:02:50 +0200

    error handling refactor

--- src/main/java/parser/WhileProgramVisitor.java
+++ src/main/java/parser/WhileProgramVisitor.java
@@ -1,103 +1,165 @@
 package parser;
 
+import org.antlr.v4.runtime.ParserRuleContext;
+import org.antlr.v4.runtime.misc.Interval;
 import org.antlr.v4.runtime.tree.ErrorNode;
 import org.antlr.v4.runtime.tree.TerminalNode;
 import program.IProgramElement;
-import program.expressions.BinOp;
-import program.expressions.Identifier;
-import program.expressions.UnOp;
-import program.expressions.Value;
+import program.expressions.*;
 import program.statements.*;
 import syntax.while_parser.WhileBaseVisitor;
 import syntax.while_parser.WhileParser;
 
 import java.math.BigInteger;
-
-import static parser.WhileProgramParserHelper.*;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.function.Function;
+import java.util.function.Supplier;
 
 
 public class WhileProgramVisitor extends WhileBaseVisitor<IProgramElement> {
 
-    private WhileProgramParserHelper helper = new WhileProgramParserHelper(this);
+    private static final String MISSING = "missing";
+    private static final String MISSING_STATEMENT = "<missing statement>";
+    private static final String MISSING_EXPRESSION = "<missing expression>";
+
+    private static final Map<Integer, BinOp.Arithmetic> ARITHMETIC_BIN_OPS = new HashMap<>();
+
+    static {
+        ARITHMETIC_BIN_OPS.put(WhileParser.DIV, BinOp.Arithmetic.DIV);
+        ARITHMETIC_BIN_OPS.put(WhileParser.MUL, BinOp.Arithmetic.MUL);
+        ARITHMETIC_BIN_OPS.put(WhileParser.REM, BinOp.Arithmetic.REM);
+        ARITHMETIC_BIN_OPS.put(WhileParser.PLUS, BinOp.Arithmetic.ADD);
+        ARITHMETIC_BIN_OPS.put(WhileParser.MINUS, BinOp.Arithmetic.SUB);
+    }
+
+    private static final Map<Integer, BinOp.Relational> RELATIONAL_BIN_OPS = new HashMap<>();
+
+    static {
+        RELATIONAL_BIN_OPS.put(WhileParser.LT, BinOp.Relational.LT);
+        RELATIONAL_BIN_OPS.put(WhileParser.LE, BinOp.Relational.LE);
+        RELATIONAL_BIN_OPS.put(WhileParser.GT, BinOp.Relational.GT);
+        RELATIONAL_BIN_OPS.put(WhileParser.GE, BinOp.Relational.GE);
+        RELATIONAL_BIN_OPS.put(WhileParser.EQ, BinOp.Relational.EQ);
+        RELATIONAL_BIN_OPS.put(WhileParser.NE, BinOp.Relational.NE);
+    }
+
+    private static final Map<Integer, BinOp.Logical> LOGICAL_BIN_OPS = new HashMap<>();
+
+    static {
+        LOGICAL_BIN_OPS.put(WhileParser.AND, BinOp.Logical.AND);
+        LOGICAL_BIN_OPS.put(WhileParser.OR, BinOp.Logical.OR);
+        LOGICAL_BIN_OPS.put(WhileParser.XOR, BinOp.Logical.XOR);
+    }
+
+    private static boolean isUnrecoverable(ParserRuleContext ctx) {
+        return ctx == null || ctx.getStart().getStartIndex() > ctx.getStop().getStopIndex();
+    }
+
+    private static String getErrorText(ParserRuleContext ctx) {
+        return ctx.getStart().getInputStream().getText(new Interval(ctx.getStart().getStartIndex(), ctx.getStop().getStopIndex()));
+    }
+
+    private static <T extends IProgramElement> T handleVisit(ParserRuleContext ctx, Supplier<T> supplier, Function<String, T> errorCtor, String missingMsg) {
+        if (isUnrecoverable(ctx)) {
+            return errorCtor.apply(missingMsg);
+        }
+        if (containsPartialError(ctx)) {
+            return errorCtor.apply(getErrorText(ctx));
+        }
+        return supplier.get();
+    }
+
+    private static boolean containsPartialError(ParserRuleContext ctx) {
+        return ctx.exception != null || ctx.getText().contains(MISSING);
+    }
+
+    private static IStatement handleStmVisit(ParserRuleContext ctx, Supplier<IStatement> supplier) {
+        return handleVisit(ctx, supplier, StatementSyntaxError::new, MISSING_STATEMENT);
+    }
+
+    private static IExpression handleExprVisit(ParserRuleContext ctx, Supplier<IExpression> supplier) {
+        return handleVisit(ctx, supplier, ExpressionSyntaxError::new, MISSING_EXPRESSION);
+    }
 
     @Override
     public IProgramElement visitAssignment(WhileParser.AssignmentContext ctx) {
-        return helper.makeStm(ctx, Assignment::new, ctx.expr(0), ctx.expr(1));
+        return handleStmVisit(ctx, () -> new Assignment((IExpression) visit(ctx.expr(0)), (IExpression) visit(ctx.expr(1))));
     }
 
     @Override
     public IProgramElement visitPar(WhileParser.ParContext ctx) {
-        return helper.makeStm(ctx, Par::new, ctx.stm(0), ctx.stm(1));
+        return handleStmVisit(ctx, () -> new Par((IStatement) visit(ctx.stm(0)), (IStatement) visit(ctx.stm(1))));
     }
 
     @Override
     public IProgramElement visitAbort(WhileParser.AbortContext ctx) {
-        return new Abort();
+        return handleStmVisit(ctx, Abort::new);
     }
 
     @Override
     public IProgramElement visitOr(WhileParser.OrContext ctx) {
-        return helper.makeStm(ctx, Or::new, ctx.stm(0), ctx.stm(1));
+        return handleStmVisit(ctx, () -> new Or((IStatement) visit(ctx.stm(0)), (IStatement) visit(ctx.stm(1))));
     }
 
     @Override
     public IProgramElement visitSkip(WhileParser.SkipContext ctx) {
-        return new Skip();
+        return handleStmVisit(ctx, Skip::new);
     }
 
     @Override
     public IProgramElement visitWhile(WhileParser.WhileContext ctx) {
-        return helper.makeStm(ctx, While::new, ctx.expr(), ctx.stm());
+        return handleStmVisit(ctx, () -> new While((IExpression) visit(ctx.expr()), (IStatement) visit(ctx.stm())));
     }
 
     @Override
     public IProgramElement visitSequence(WhileParser.SequenceContext ctx) {
-        return helper.makeStm(ctx, Sequence::new, ctx.stm(0), ctx.stm(1));
+        return handleStmVisit(ctx, () -> new Sequence((IStatement) visit(ctx.stm(0)), (IStatement) visit(ctx.stm(1))));
     }
 
     @Override
     public IProgramElement visitIf(WhileParser.IfContext ctx) {
-        return helper.makeStm(ctx, If::new, ctx.expr(), ctx.stm(0), ctx.stm(1));
+        return handleStmVisit(ctx, () -> new If((IExpression) visit(ctx.expr()), (IStatement) visit(ctx.stm(0)), (IStatement) visit(ctx.stm(1))));
     }
 
     @Override
     public IProgramElement visitNot(WhileParser.NotContext ctx) {
-        return helper.makeExpr(ctx, UnOp.Logical.NOT::of, ctx.expr());
+        return handleExprVisit(ctx, () -> UnOp.Logical.NOT.of((IExpression) visit(ctx.expr())));
     }
 
     @Override
     public IProgramElement visitAddSub(WhileParser.AddSubContext ctx) {
-        return helper.makeExpr(ctx, ARITHMETIC_BIN_OPS.get(ctx.op.getType())::of, ctx.expr(0), ctx.expr(1));
+        return handleExprVisit(ctx, () -> ARITHMETIC_BIN_OPS.get(ctx.op.getType()).of((IExpression) visit(ctx.expr(0)), (IExpression) visit(ctx.expr(1))));
     }
 
     @Override
     public IProgramElement visitAnd(WhileParser.AndContext ctx) {
-        return helper.makeExpr(ctx, BinOp.Logical.AND::of, ctx.expr(0), ctx.expr(1));
+        return handleExprVisit(ctx, () -> BinOp.Logical.AND.of((IExpression) visit(ctx.expr(0)), (IExpression) visit(ctx.expr(1))));
     }
 
     @Override
     public IProgramElement visitOrXor(WhileParser.OrXorContext ctx) {
-        return helper.makeExpr(ctx, LOGICAL_BIN_OPS.get(ctx.op.getType())::of, ctx.expr(0), ctx.expr(1));
+        return handleExprVisit(ctx, () -> LOGICAL_BIN_OPS.get(ctx.op.getType()).of((IExpression) visit(ctx.expr(0)), (IExpression) visit(ctx.expr(1))));
     }
 
     @Override
     public IProgramElement visitMulDivRem(WhileParser.MulDivRemContext ctx) {
-        return helper.makeExpr(ctx, ARITHMETIC_BIN_OPS.get(ctx.op.getType())::of, ctx.expr(0), ctx.expr(1));
+        return handleExprVisit(ctx, () -> ARITHMETIC_BIN_OPS.get(ctx.op.getType()).of((IExpression) visit(ctx.expr(0)), (IExpression) visit(ctx.expr(1))));
     }
 
     @Override
     public IProgramElement visitMinus(WhileParser.MinusContext ctx) {
-        return helper.makeExpr(ctx, UnOp.Arithmetic.NEG::of, ctx.expr());
+        return handleExprVisit(ctx, () -> UnOp.Arithmetic.NEG.of((IExpression) visit(ctx.expr())));
     }
 
     @Override
     public IProgramElement visitRel1(WhileParser.Rel1Context ctx) {
-        return helper.makeExpr(ctx, RELATIONAL_BIN_OPS.get(ctx.op.getType())::of, ctx.expr(0), ctx.expr(1));
+        return handleExprVisit(ctx, () -> RELATIONAL_BIN_OPS.get(ctx.op.getType()).of((IExpression) visit(ctx.expr(0)), (IExpression) visit(ctx.expr(1))));
     }
 
     @Override
     public IProgramElement visitRel2(WhileParser.Rel2Context ctx) {
-        return helper.makeExpr(ctx, RELATIONAL_BIN_OPS.get(ctx.op.getType())::of, ctx.expr(0), ctx.expr(1));
+        return handleExprVisit(ctx, () -> RELATIONAL_BIN_OPS.get(ctx.op.getType()).of((IExpression) visit(ctx.expr(0)), (IExpression) visit(ctx.expr(1))));
     }
 
     @Override
@@ -107,34 +169,28 @@ public class WhileProgramVisitor extends WhileBaseVisitor<IProgramElement> {
 
     @Override
     public IProgramElement visitInteger(WhileParser.IntegerContext ctx) {
-        return new Value<>(new BigInteger(ctx.getText()));
+        return handleExprVisit(ctx, () -> new Value<>(new BigInteger(ctx.getText())));
     }
 
     @Override
     public IProgramElement visitBool(WhileParser.BoolContext ctx) {
-        return new Value<>(ctx.tf.getType() == WhileParser.TRUE);
+        return handleExprVisit(ctx, () -> new Value<>(ctx.tf.getType() == WhileParser.TRUE));
     }
 
     @Override
     public IProgramElement visitIdentifier(WhileParser.IdentifierContext ctx) {
-        return new Identifier(ctx.getText());
+        return handleExprVisit(ctx, () -> new Identifier(ctx.getText()));
     }
 
     @Override
     public IProgramElement visitStart(WhileParser.StartContext ctx) {
-
-        if (isErrorous(ctx)) {
-            return new StatementSyntaxError(MISSING_STATEMENT);
-        }
-        return visit(ctx.s);
+        return handleStmVisit(ctx, () -> (IStatement) visit(ctx.s));
     }
 
     @Override
     public IProgramElement visitAtomExpr(WhileParser.AtomExprContext ctx) {
-        return visit(ctx.atom());
+        return handleExprVisit(ctx, () -> (IExpression) visit(ctx.atom()));
     }
-
-
 
     @Override
     public IProgramElement visitTerminal(TerminalNode terminalNode) {
